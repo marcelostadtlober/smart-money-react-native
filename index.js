34ac0faa0b1567efe4b0f2f981ee0fbb06125761
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2021-04-21 02:03:04 -0300

    Criando a tela principal

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './src';
+import Main from './src/pages/Main'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Main);
